4df4eb99001a903365de38c4a80ab321a32c572b
ihollander <ianholla@gmail.com>
2020-12-27 09:48:04 -0500

    Updated solution

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,21 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function EyesOnMe() {
-
   function handleFocus() {
-    console.log("Good!")
+    console.log("Good!");
   }
 
   function handleBlur() {
-    console.log("Hey! Eyes on me!")
+    console.log("Hey! Eyes on me!");
   }
 
   return (
-    <button 
-      onFocus={handleFocus}
-      onBlur={handleBlur}
-    >Click me</button>
-  )
+    <button onFocus={handleFocus} onBlur={handleBlur}>
+      Eyes on me
+    </button>
+  );
 }
 
-export default EyesOnMe
+export default EyesOnMe;
