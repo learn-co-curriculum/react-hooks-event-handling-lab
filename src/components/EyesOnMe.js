71a13430f5d4cf173e2c95b1af9929726ac537ad
Edward <jaysongraphics@gmail.com>
2021-06-10 09:15:33 -0400

    commit tree 8c10dc618f70dca4d3d8a2c17ff12a939140bd58

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+
+
+
+function EyesOnMe(){
+    function veryFocus (e){
+        console.log('Good!')
+    }
+
+    function veryBlurry(e){
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (
+        <button onFocus={veryFocus} onBlur={veryBlurry}>
+           Eyes on me
+        </button>
+    )
+}
+
+
+
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+
+
+function Keypad(){
+    function changeEven(e){
+        // console.log(e.target.value)
+        console.log('Entering password...')
+    }
+    return (
+        <input type="password" onChange={changeEven} />
+    )
+}
+
+
+export default Keypad;
