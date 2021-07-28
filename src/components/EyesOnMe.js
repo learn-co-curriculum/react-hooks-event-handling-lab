d362595a5ed9ab28fc20f4122b029223bd4d6577
Zahraa <sahrayarub@yahoo.de>
2021-07-28 11:38:18 +0300

    commit tree f845d563a2c37a510d450084a4407ec6b2adcdd8

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+const focusHandler = () => {
+    console.log('Good!')
+}
+
+const blurHandler = () => {
+    console.log('Hey! Eyes on me!')
+}
+
+
+function EyesOnMe() {
+    return (
+        <div>
+            <button onFocus={focusHandler} onBlur={blurHandler}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React from 'react'
+
+const changeHandler = () => {
+    console.log('Entering password...')
+}
+function Keypad() {
+    return (
+        <div>
+            <input type="password" onChange={changeHandler}/>
+        </div>
+    )
+}
+
+export default Keypad
