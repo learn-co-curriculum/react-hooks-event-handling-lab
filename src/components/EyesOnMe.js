6347f9e41d51cd6ecd052e3b59660d1dd8df87f6
david_venegas <venegasdavidm@gmail.com>
2021-11-03 14:33:42 -0400

    commit tree 9b45f194b0b42f45feeb6a41570a31f2a04231fa

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
-// Code EyesOnMe Component Here
+function EyesOnMe() {
+    function blur() {
+        console.log('Hey! Eyes on me!');
+    }
+    function focus() {
+        console.log('Good!');
+    }
+
+    return <button onBlur={blur} onFocus={focus}>Eyes on me</button>
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
-// Code Keypad Component Here
+import React from 'react'
+
+function Keypad() {
+    function password() {
+        console.log('Entering password...');
+    }
+
+    return <input type="password" onChange={password}></input>
+}
+
+export default Keypad
