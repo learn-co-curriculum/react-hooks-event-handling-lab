581beb1e55d186ddee83c995e8f8e9145986c90a
Tywen Kelly <tywen.kelly@gmail.com>
2022-02-23 18:20:22 -0800

    finished lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from "react"
+
+function EyesOnMe() {
+  function handleOnBlur() {
+    console.log("Hey! Eyes on me!")
+  }
+
+  function handleOnFocus() {
+    console.log("Good!")
+  }
+
+  return (
+    <button onFocus={handleOnFocus} onBlur={handleOnBlur}>
+      Eyes on me
+    </button>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+import React from "react"
+
+function onChange() {
+  console.log("Entering password...")
+}
+
+function Keypad() {
+  return <input type="password" onChange={onChange} />
+}
+
+export default Keypad
