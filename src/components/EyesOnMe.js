4524f8c440c978265663aa527d2b3436e0232e90
maclaine <maclainekuria@gmail.com>
2022-07-16 17:55:53 +0300

    commit tree d4c661558ae8894867a698a419a18ee54f482274

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+  return(
+    <div>
+      <button 
+      onBlur={()=> console.log('Hey! Eyes on me!')} 
+      onFocus={()=> console.log('Good!')}
+      >Eyes on me</button>
+    </div>
+  ) 
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React from 'react'
+
+function Keypad() {
+  return (
+    <div>
+      <input
+      onChange={()=> console.log('Entering password...')} 
+      type='password' 
+      />
+    </div>
+  )
+}
+
+export default Keypad
